3aa8341af6dfa41d8b2ef8d320bfd5b1973abf2d
manishsinghkuswaha <47105557+manishsinghkuswaha@users.noreply.github.com>
2021-06-24 14:20:23 +0530

    Create height_diameter_tree.cpp

---
+++ height_diameter_tree.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+    Node(int val){
+    data=val;
+    left=NULL;
+    right=NULL;
+    }
+};
+
+int height(Node* root)
+{
+    if(root==NULL){
+        return 0;
+    }
+    int left=height(root->left);
+    int right=height(root->right);
+    return max(left,right)+1;
+}
+
+
+
+int main(){
+    struct Node* root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+    root->right->left = new Node(6);
+    root->right->right = new Node(7);
+    cout<<height(root)<<endl;
+    return 0;
+}
